b54d4af6626a758a4e71a316c6fbaa0eedc703d9
unknown <kaatz@sf007.FRIENDS>
2012-03-16 10:56:28 +0100

    URLs für Tag-Filterung

--- demos/json-events.php
+++ demos/json-events.php
@@ -1,6 +1,13 @@
 <?php
 
-	$ch = curl_init("http://feg.kaatz-media.de:9000/events");
+	url = "http://feg.kaatz-media.de:9000/events";
+
+	if($_GET['tag'])
+		$ch = curl_init(url+"/tag/"+$_GET['tag']);
+	else 
+		$ch = curl_init(url);
+
+	
 	curl_exec($ch);
 	curl_close($ch);
 
