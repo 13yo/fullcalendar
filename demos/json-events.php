beb8ba9844b1d343bc7e03b1719f02efe4ad4920
unknown <kaatz@sf007.FRIENDS>
2012-03-16 11:12:56 +0100

    debug2

--- demos/json-events.php
+++ demos/json-events.php
@@ -3,11 +3,11 @@
 	$url = "http://feg.kaatz-media.de:9000/events";
 
 	if($_GET["tag"])
-		$ch = curl_init($url+"/tag/"+$_GET["tag"]);
+		$ch = curl_init($url."/tag/".$_GET["tag"]);
 	else 
 		$ch = curl_init($url);
 
-	println($url+"/tag/"+$_GET["tag"]);
+	echo($url."/tag/".$_GET["tag"]);
 
 	curl_exec($ch);
 	curl_close($ch);
